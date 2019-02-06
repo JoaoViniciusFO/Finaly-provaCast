6c9678708fded08e0f325bbced6f1b4c1668705c
joao <joao.vinicius@maisvida.med.br>
2019-02-06 10:55:15 -0200

    Setting new root to swagger doc

--- src/br/com/servico/AtivaService.java
+++ src/br/com/servico/AtivaService.java
@@ -5,6 +5,11 @@ import java.util.Set;
 
 import javax.ws.rs.core.Application;
 
+import br.com.entity.Conta;
+import br.com.servico.Service;
+import io.swagger.jaxrs.config.BeanConfig;
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
 
 public class AtivaService extends Application {
 
@@ -13,20 +18,39 @@ public class AtivaService extends Application {
 	private Set<Class<?>> empty = new HashSet<Class<?>>();
 
 	public AtivaService() {
+		//para swagger.
+		BeanConfig conf = new BeanConfig();
+		conf.setTitle("Criação e busca de contas");
+		conf.setDescription("API para persistencia e busca de dados bancarios do sistema.");
+		conf.setVersion("1.0.0");
+		conf.setHost("localhost:8080");
+		conf.setBasePath("/TesteWeb-0.0.1-SNAPSHOT/main/swagger/");
+		conf.setSchemes(new String[] { "http" });
+		conf.setResourcePackage("br.com.servico");
+		conf.setScan(true);
 
 		singletons.add(new Service());
 
 	}
 
-	public Set<Object> getSingletons() {
+	//para o swagger.
+	@Override
+	public Set<Class<?>> getClasses() {
+		Set<Class<?>> resources = new HashSet();
+		resources.add(Conta.class);
 
+		// classes do swagger...
+		resources.add(ApiListingResource.class);
+		resources.add(SwaggerSerializers.class);
+		return resources;
+	}
+
+	public Set<Object> getSingletons() {
 		return singletons;
 
 	}
 
 	public Set<Class<?>> getEmpty() {
-
 		return empty;
-
 	}
 }
